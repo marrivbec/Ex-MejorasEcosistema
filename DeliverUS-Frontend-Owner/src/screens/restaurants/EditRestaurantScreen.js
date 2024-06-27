a169cd0d7d52995f431edf8848ec21e421825532
Mario Rivas Becerra <rivasmario013@gmail.com>
2024-06-27 10:44:21 +0200

    Solution

--- DeliverUS-Frontend-Owner/src/screens/restaurants/EditRestaurantScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/EditRestaurantScreen.js
@@ -203,6 +203,24 @@ export default function EditRestaurantScreen ({ navigation, route }) {
               />
               <ErrorMessage name={'restaurantCategoryId'} render={msg => <TextError>{msg}</TextError> }/>
 
+              <Pressable
+                onPress={() => navigation.navigate('CreateRestaurantCategoryScreen')}
+                style={({ pressed }) => [
+                  {
+                    backgroundColor: pressed
+                      ? GlobalStyles.brandGreenTap
+                      : GlobalStyles.brandGreen
+                  },
+                  styles.button
+                ]}>
+                <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+                  <MaterialCommunityIcons name='folder-plus-outline' color={'white'} size={20} />
+                  <TextRegular textStyle={styles.text}>
+                    New category
+                  </TextRegular>
+                </View>
+              </Pressable>
+
               <Pressable onPress={() =>
                 pickImage(
                   async result => {
